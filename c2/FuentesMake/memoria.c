2b93806b2ac854e8002803f1fb3a1450f49977ce
unknown <aroblessilva@outlook.com>
2020-03-19 20:21:03 +0100

    curso2

---
+++ c2/FuentesMake/memoria.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+#include "memoria.h"
+
+int *
+asignar_memoria (long n)
+{
+   int *mem;
+   if ( (mem = (int *) calloc(n, sizeof(int))) == NULL )
+	 printf("Error reservando memoria.\n");
+
+   return mem;
+}
+
+
+
+void
+liberar_memoria (int *la)
+{
+   free(la);    /* Liberamos la memoria reservada anteriormente */
+}
---
+++ c2/FuentesMake/metodos.h
@@ -0,0 +1,9 @@
+#ifndef __METODOS_ORD_H
+#define __METODOS_ORD_H
+
+  float ordena_burbuja   (long, int *);
+  float ordena_insercion (long, int *);
+  float ordena_seleccion (long, int *);
+
+
+#endif
